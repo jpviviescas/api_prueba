c0488c1385534a915a93e8d4e25726f7bf0b19ce
Juan Pablo Viviescas <jpviviescas@losmoviles.com.co>
2023-09-08 14:49:37 -0500

    list pokemons finalized

--- src/prueba/context/components/getPokemonList.vue
+++ src/prueba/context/components/getPokemonList.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="backgroundGetPokemonList"></div>
+  <div class="backgroundGetPokemonList">
+    <div class="pokemonList">
+      <div v-for="(item, index) in pokemonList" :key="index" class="pokemon">
+        <img :src="imgSrc(index + 1)" class="PokemonImage" />
+        <p class="pokemonText">{{ item.name }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import axios from "axios";
@@ -9,22 +16,47 @@ export default {
   data() {
     return {
       pokemonList: null,
-      pokemonName: null,
-      id: "6",
     };
   },
   async mounted() {
     await axios
       .get("https://pokeapi.co/api/v2/pokemon/")
-      .then((response) => (this.pokemonName = response));
-    console.log(this.pokemonName);
+      .then((response) => (this.pokemonList = response.data.results));
+  },
+  methods: {
+    imgSrc(id) {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+    },
   },
 };
 </script>
 <style scoped>
 .backgroundGetPokemonList {
-  background-color: aqua;
   width: 100%;
-  height: 90vh;
+  height: 95vh;
+}
+.backgroundGetPokemonList .pokemonList {
+  /* background-color: blue; */
+  width: 100%;
+  height: 95vh;
+  display: grid;
+  grid-template-rows: repeat(5, 1fr);
+  grid-template-columns: repeat(4, 1fr);
+  gap: 2vh;
+}
+.backgroundGetPokemonList .pokemonList .pokemon {
+  /* background-color: cadetblue; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.pokemon .PokemonImage {
+  width: 100px;
+  height: 100px;
+}
+.pokemon .pokemonText {
+  color: azure;
+  font-size: 3vh;
 }
 </style>
