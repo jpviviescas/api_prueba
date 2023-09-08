1c263d4e3f62e835dad44d34bdd58b5a0df75acb
Juan Pablo Viviescas <jpviviescas@losmoviles.com.co>
2023-09-08 15:27:03 -0500

    add pagination component

---
+++ src/prueba/context/components/Pagination.vue
@@ -0,0 +1,7 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default { name: "paginationVue" };
+</script>
+<style scoped></style>
---
+++ src/prueba/context/views/PokemonPagiation.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="backgroundPokemonPagination"><p class="title">Pagination</p></div>
+</template>
+<script>
+export default { name: "PokemonPagination" };
+</script>
+<style scoped>
+.backgroundPokemonPagination {
+  background-color: #d22e2e;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.backgroundPokemonPagination .title {
+  font-size: 5vh;
+  color: white;
+}
+</style>
